59c69e57b85842443d5f9d5ba320e63fd28cd8d1
Leandro Silva <leandro.jj.silva@gmail.com>
2024-12-04 19:52:26 -0300

    modified:   javascript.js     Add playRound function and its logic     Add playGame function and its logic     Add validation and while loop to getHumanChoice     function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
 
 let getComputerChoice = ()=>{
     let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
@@ -21,12 +23,58 @@ let getComputerChoice = ()=>{
 }
 
 function getHumanChoice(){
-   let humanChoice = prompt("Enter your choice: rock, paper or scissors.").toLowerCase();
-    if(humanChoice!=="rock" && humanChoice!=="paper" && humanChoice!=="scissors"){
-        console.error("please choose one of the given options");
-    }
+    let humanChoice="";
+    
+    while(humanChoice===""){
+            humanChoice = prompt("Enter your choice: rock, paper or scissors.").toLowerCase();
+            if(humanChoice!=="rock" && humanChoice!=="paper" && humanChoice!=="scissors"){
+                console.error("please choose one of the given options");
+                humanChoice = "";
+            }
+    }    
+    
     return humanChoice;
 }
 
-getComputerChoice();
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice===computerChoice){
+     console.log(`Draw!`);
+    }else if(humanChoice==="rock" && computerChoice=="paper"){
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }else if(humanChoice==="paper" && computerChoice=="scissors"){
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }else if(humanChoice==="scissors" && computerChoice=="rock"){
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }else {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+        }
+}
+
+
+function playGame(){
+   
+    for(let i = 0; i<=4; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+
+        console.log(`   Computer score: ${computerScore} 
+            Your score: ${humanScore}`);
+    }
+    if(humanScore>computerScore){
+        console.log(`Congratulations you won: ${humanScore} rounds! You win the game`);
+    } else if(computerScore>humanScore){
+        console.log(`Sorry you lost: ${computerScore} rounds! You lost the game`);
+    } else{
+        console.log(`Draw`);
+    }
+
+}
+
+playGame();
